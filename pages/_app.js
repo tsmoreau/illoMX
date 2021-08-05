d948c1cb1ce8c903e01ac6e11597e777de473b13
tsmoreau <64439028+tsmoreau@users.noreply.github.com>
2021-08-05 07:38:32 -0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -6,19 +6,38 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <nav className="border-b p-6">
-        <p className="text-4xl font-bold text-center">illoMX</p>
+        <div class="flex-wrap mt-4 content-center text-center items-center">
+          <p class="text-4xl font-bold text-center inline-flex">
+            {" "}
+            <a href="#" class="">
+              illoMX
+            </a>
+          </p>{" "}
+          <p class="inline-flex text-xs italic font-sans font-normal tracking-tighter text-gray-300">
+            alpha
+          </p>
+        </div>
+
         <div className="flex-wrap mt-4 content-center text-center items-center">
           <Link href="/">
-            <a className="mr-4 text-green-600">market</a>
+            <a className="mr-4 text-green-700 tracking-tight text-semi-bold">
+              market
+            </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-green-600">create NFT</a>
+            <a className="mr-6 text-green-700 tracking-tight text-semi-bold">
+              create NFT
+            </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-green-600">dashboard</a>
+            <a className="mr-6 text-green-700 tracking-tight text-semi-bold">
+              dashboard
+            </a>
           </Link>
           <Link href="/faq">
-            <a className="mr-6 text-green-600">faq</a>
+            <a className="mr-6 text-green-700 tracking-tight text-semi-bold">
+              faq
+            </a>
           </Link>
         </div>
       </nav>
